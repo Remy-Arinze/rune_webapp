72631337a2884682d9ae3d454af74b73ee7b5c4c
Remy <68857172+Remy-Arinze@users.noreply.github.com>
2025-05-19 12:25:06 +0100

    Add Pexels domain to Next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["airnfts.s3.amazonaws.com"], // Add your domain here
+		domains: ["airnfts.s3.amazonaws.com", "images.pexels.com"], // Added Pexels domain
 	},
 };
 
